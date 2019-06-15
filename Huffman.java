9b97b58f7b0a9e8d353fe87ac140abbe350f147e
hvatitorat <ielectro.com.com@gmail.com>
2019-06-15 12:23:13 +0300

    second commit

--- Huffman.java
+++ Huffman.java
@@ -2,7 +2,26 @@ package com.alevel.moduleTwo;
 
 public class Huffman {
     int alphabetSize = 256;
-    Tree tree[] = new Tree[alphabetSize]; //массив деревьев
-    int weights[] = new int[alphabetSize]; //массив частоты символов
+    Tree tree[] = new Tree[alphabetSize];   //массив деревьев
+    int weights[] = new int[alphabetSize];  //массив частоты символов
+    public String code[] = new String[alphabetSize];    //конечный код Хаффмана
+
+
+    private int getLowestTree(int used) {
+        int min = 0;
+        for (int i = 1; i < used; i++) {
+            if (tree[i].weight < tree[min].weight) {
+                min = i;
+            }
+        }
+        return min;
+    }
+
+    public void growTree(int[] data) {
+        for (int i = 0; i < data.length; i++) {     //считает частоту символов
+            weights[data[i]]++;
+        }
+        int used = 0;
+    }
 
 }
