dcf0c1ed464ebf931d3ada262f7241f32f113cd2
hvatitorat <ielectro.com.com@gmail.com>
2019-06-15 12:08:46 +0300

    first commit

--- Huffman.java
+++ Huffman.java
@@ -1,4 +1,8 @@
 package com.alevel.moduleTwo;
 
 public class Huffman {
+    int alphabetSize = 256;
+    Tree tree[] = new Tree[alphabetSize]; //массив деревьев
+    int weights[] = new int[alphabetSize]; //массив частоты символов
+
 }
